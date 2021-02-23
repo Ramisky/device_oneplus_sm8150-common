8267322b61bea2e85b14af28ee6470ce10ffee5f
DigiGoon <kagatharatarang@gmail.com>
2021-02-23 18:40:19 +0530

    sm8150-common: drop Snap & build GCamGO
    Signed-off-by: DigiGoon <kagatharatarang@gmail.com>

--- common.mk
+++ common.mk
@@ -180,7 +180,7 @@ PRODUCT_PACKAGES += \
     libxml2 \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.oneplus.camera.CameraHIDL@1.0.vendor \
-    Snap
+    GCamGOPrebuilt
 
 # CNE
 PRODUCT_PACKAGES += \
